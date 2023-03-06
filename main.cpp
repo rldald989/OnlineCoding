ce87a751c874ac701f242b7529f4f9830831426c
rldald989 <70861085+rldald989@users.noreply.github.com>
2023-03-06 22:35:26 +0000

    It's a console emulator now

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <stdlib.h>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    bool running = true;
+    std::string input;
+
+    while(running)
+    {
+        std::getline(std::cin, input);
+        if(input == "cls")
+        {
+            system("cls");
+        }
+    }
 }
