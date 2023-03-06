6bdc8a95764da84e2c9ecac7643762f633b7998d
rldald989 <70861085+rldald989@users.noreply.github.com>
2023-03-06 15:56:16 -0800

    Changes to make the help command work

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
+#include <string>
 #include <stdlib.h>
+
+std::string commands[] = 
+{
+    "help - lists the current commands\n",
+    "cls - clears the screen\n",
+    "dir - outputs the directory\n",
+    "credits - shows who has worked on this project\n",
+    "exit - exits the program\n"
+};
 
 int main(int argc, char* argv[])
 {
     bool running = true;
     std::string input;
 
-    std::string commands[] = 
-    {
-        "help - lists the current commands\n",
-        "cls - clears the screen\n",
-        "dir - outputs the directory\n",
-        "credits - shows who has worked on this project\n",
-        "exit - exits the program\n"
-    };
-
     while(running)
     {
         printf("user>");
         std::getline(std::cin, input);
-        if(input == "help")
+
+        if(input == "")
+        {
+
+        }
+        else if(input == "help")
         {
             for(int i = 0; i < commands->size(); i++)
             {
@@ -28,7 +34,7 @@ int main(int argc, char* argv[])
         }
         else if(input == "cls")
         {
-            system("clear");
+            system("cls");
         }
         else if(input == "dir")
         {
